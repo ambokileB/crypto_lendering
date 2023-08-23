fc6e1a87b83a00b65fece54b948d523d3840a61c
ERDWARDCHUMA <vitallyemc@gmail.com>
2023-08-23 22:46:42 +0300

    initial commit

---
+++ src/main/java/com/ambokileworkingcode/onlineservices/OnlineServicesApplication.java
@@ -0,0 +1,13 @@
+package com.ambokileworkingcode.onlineservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ambokileworkingcode/onlineservices/OnlineServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ambokileworkingcode.onlineservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
